77b1ea71e2fa88c4020bede10a1d47998f161ada
cqm1994617 <1025760508@qq.com>
2020-03-14 00:40:06 +0800

    'save'

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
 
     return (
       <BrowserRouter basename="/page-builder">
-        <div>123</div>
+        <div>12</div>
       </BrowserRouter>
     )
   }
