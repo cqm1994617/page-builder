ed3efd112113d745daaaaf04f07a4283a32bc7f8
cqm1994617 <1025760508@qq.com>
2020-03-23 18:09:37 +0800

    '基础UI'

---
+++ src/pages/edit/components/SandBox/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Viewer = styled.div`
+  margin: 40px auto 0;
+  width: 375px;
+  min-height: 667px;
+  background-color: #fff;
+`
+
+function SandBox() {
+  return (
+    <Viewer>
+
+    </Viewer>
+  )
+}
+
+export default SandBox
