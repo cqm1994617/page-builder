bb448e754b4ea8b16d5ba046687055194359443e
cqm1994617 <1025760508@qq.com>
2020-04-01 01:49:41 +0800

    '打包测试文件修改'

--- src/server/createFile.js
+++ src/server/createFile.js
@@ -5,7 +5,7 @@ const config = require('./webpack-dynamic')
 const codeStr = `
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Banner from '../component-list/banner/display/index'
+import {BannerServer as Banner} from '../component-list/banner'
 
 const list = [{"name":"Banner","key":"6410b73d-afd0-4641-863c-ef725585b70e","props":{"bannerList":[{"imgUrl":"http://qiniu.xingheaoyou.com/1.jpg","to":"https://www.baidu.com"},{"imgUrl":"http://qiniu.xingheaoyou.com/2.jpg","to":"https://www.taobao.com"}],"height":"200px"}},{"name":"Banner","key":"9d7a3517-6030-45ff-8dc7-6543b9e59d09","props":{"bannerList":[{"imgUrl":"http://qiniu.xingheaoyou.com/1.jpg","to":"https://www.baidu.com"},{"imgUrl":"http://qiniu.xingheaoyou.com/2.jpg","to":"https://www.taobao.com"}],"height":"300px"}},{"name":"Banner","key":"df31eb60-ef90-4a25-a3de-b8e730c622d2","props":{"bannerList":[{"imgUrl":"http://qiniu.xingheaoyou.com/1.jpg","to":"https://www.baidu.com"},{"imgUrl":"http://qiniu.xingheaoyou.com/2.jpg","to":"https://www.taobao.com"}],"height":"200px"}}]
 
@@ -13,8 +13,8 @@ function App() {
   return (
     <div style={{overflow: 'hidden'}}>
       {
-        list.map((item, index) => {
-          return <Banner key={index} {...item.props} />
+        list.map((item) => {
+          return <Banner key={item.key} {...item.props} />
         })
       }
     </div>
