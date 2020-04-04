a526f55afbdf6c14a40ba1833cb6bf9e99c79756
cqm1994617 <1025760508@qq.com>
2020-04-04 20:49:23 +0800

    'save'

--- src/client/App.js
+++ src/client/App.js
@@ -1,13 +1,16 @@
 import React, { lazy, Suspense, useState, useEffect } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import reducers from './reducer'
 
 const Edit = lazy(() => import('./pages/edit/index'))
-
-const store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, composeEnhancers(
+    applyMiddleware(thunk)
+  )
+)
 
 const Loading = () => <div>加载中</div>
 
