cf47a284b46c99d31ac1df5780c3f0de057ac74e
cqm1994617 <1025760508@qq.com>
2020-05-11 23:28:09 +0800

    save

---
+++ src/component-list/image/index.js
@@ -0,0 +1,11 @@
+import ImageClient from './client'
+import ImageServer from './server'
+import ToolPanel from './tool'
+import componentItem from './componentItem'
+
+export {
+  ImageClient,
+  ImageServer,
+  ToolPanel,
+  componentItem
+}
