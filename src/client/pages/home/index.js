656d17cdb28a894c54e58178bee14c6ab87ba67c
cqm1994617 <1025760508@qq.com>
2020-05-21 20:59:56 +0800

    首页状态reducer

--- src/client/pages/home/index.js
+++ src/client/pages/home/index.js
@@ -1,33 +1,92 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
 import styled from 'styled-components'
 import { useHistory } from 'react-router-dom'
+import { List, Button } from 'antd'
 
 const Page = styled.div`
   width: 100%;
   height: 100vh;
   background-color: #f5f5f5;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  overflow: hidden;
 `
-
-const Content = styled.div `
+const Content = styled.div`
   width: 1000px;
+  margin: 40px auto;
+  box-sizing: border-box;
+  padding: 0 20px;
   background-color: #fff;
 `
+const Header = styled.div`
+  padding: 30px 0 20px 0;
+`
+const EditText = styled.div`
+  color: #1890ff;
+  cursor: pointer;
+`
 
+const listReducer = (state, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'INIT':
+      return {
+        ...action.payload,
+      }
+    default: 
+      return {
+        ...state
+      }
+  }
+
+}
 
 function Home() {
 
   const history = useHistory()
+  const [state, dispatch] = useReducer(listReducer, [])
 
-  const toEdit = () => {
-    history.push('/edit')
-  }
+  useEffect(() => {
+    const projectStorage = localStorage.getItem('projectList')
+
+    try {
+      if (projectStorage) {
+        const projectList = JSON.parse(projectStorage)
+        dispatch({
+          type: 'INIT',
+          payload: projectList
+        })
+      }
+    } catch (err) {
+      console.log(err)
+      localStorage.setItem('project', null)
+    }
+
+  }, [])
 
   return (
     <Page>
-      <Content></Content>
+      <Content>
+        <Header>
+          <Button type="primary">创建应用</Button>
+          <Button type="danger" style={{marginLeft: '20px'}}>清空应用</Button>
+        </Header>
+        <List
+          dataSource={[
+            {
+              title: 'asdf',
+              description: 'fvvvv'
+            }
+          ]}
+          renderItem={item => (
+            <List.Item>
+              <List.Item.Meta
+                title={item.title}
+                description={item.description}
+              />
+              <EditText>编辑</EditText>
+            </List.Item>
+          )}
+        />
+      </Content>
     </Page>
   )
 }
