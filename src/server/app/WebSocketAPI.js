b2b48d370ef67f5a3f397dceb2315938558b2555
cqm1994617 <1025760508@qq.com>
2020-05-24 22:49:51 +0800

    '自定义的webpack插件接入websocket'

--- src/server/app/WebSocketAPI.js
+++ src/server/app/WebSocketAPI.js
@@ -3,11 +3,13 @@ const queryString = require('query-string')
 const WebSocketAPI = (wss, wsMap) => {
   wss.on('connection', function connection(ws, req) {
     const packageId = queryString.parseUrl(req.url).query.packageId
-    console.log(req.url)
-    console.log(queryString.parseUrl(req.url).query)
     wsMap[packageId] = ws
     ws.on('message', function incoming(message) {
       console.log('received: %s', message);
+    })
+    ws.on('close', () => {
+      console.log(`关闭--${packageId}`)
+      delete wsMap[packageId]
     })
     ws.send('INIT')
   })
--- src/server/package-webpack-plugin.js
+++ src/server/package-webpack-plugin.js
@@ -8,19 +8,24 @@ class PackageWebpackPlugin {
   apply(compiler) {
     compiler.hooks.entryOption.tap('entryOption', () => {
       console.log('接收到了配置', Date.now())
+      this.wsMap[this.packageId].send('entryOption')
     })
     compiler.hooks.afterPlugins.tap('afterPlugins', () => {
       console.log('plugins加载完成', Date.now())
+      this.wsMap[this.packageId].send('afterPlugins')
     })
     compiler.hooks.emit.tap('emit', () => {
       console.log('编译完成，准备生成资源', Date.now())
+      this.wsMap[this.packageId].send('emit')
     })
     compiler.hooks.emit.tap('afterEmit', () => {
       console.log('资源生成完成', Date.now())
+      this.wsMap[this.packageId].send('afterEmit')
     })
     compiler.hooks.emit.tap('done', () => {
       console.log('打包', this.wsMap)
       console.log('打包完成，总用时: ', ((Date.now() - this.time) / 1000).toFixed(1))
+      this.wsMap[this.packageId].send('done')
     })
   }
 }
