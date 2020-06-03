f558747e522c14b2a069296728f4bfe6d66a6ee5
cqm1994617 <1025760508@qq.com>
2020-06-03 21:05:44 +0800

    '注释掉browseropen'

--- src/client/config.js
+++ src/client/config.js
@@ -1,4 +1,4 @@
 export default {
-  host: 'http://localhost:9090',
-  wsHost: 'ws://localhost:9090'
+  host: 'https://cqmfe.club/page-builder-server',
+  wsHost: 'ws://cqmfe.club/page-builder-server'
 }
