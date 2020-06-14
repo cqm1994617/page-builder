9f99f8ce7af010cceb634ddfdb49a3f17c880401
cqm1994617 <1025760508@qq.com>
2020-06-14 23:37:22 +0800

    引号替换

--- src/client/config.js
+++ src/client/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   dev: {
-    host: 'http://localhost:9090',
-    wsHost: 'ws://localhost:9090'
+    host: "http://localhost:9090",
+    wsHost: "ws://localhost:9090"
   },
   production: {
-    host: 'https://cqmfe.club/page-builder-server',
-    wsHost: 'wss://cqmfe.club/page-builder-server'
+    host: "https://cqmfe.club/page-builder-server",
+    wsHost: "wss://cqmfe.club/page-builder-server"
   }
 }
